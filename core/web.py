e08c08a26ce84cf0f6a7fc8ca6297ddf4abfa97b
Lucas LaBuff <lucasbeef00@gmail.com>
2022-10-29 12:33:12 -0400

    fix oauth

--- core/web.py
+++ core/web.py
@@ -10,13 +10,19 @@ app = Flask('Ketchup')
 app.url_map.strict_slashes = False
 app.config['SERVER_NAME'] = os.getenv('SERVER_NAME', 'localhost:5000')
 app.config['SECRET_KEY'] = b64encode(os.getenv('SECRET_KEY').encode('utf-8'))
-app.config['PREFERRED_URL_SCHEME'] = 'https'
 app.jinja_env.filters['debug'] = lambda x: print(x) or ''
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = 'true' #suppresses reverse-proxy http errors - proxy should already force https
 
+# use http if running locally
+if app.config['SERVER_NAME'] == 'localhost:5000':
+    app.config['PREFERRED_URL_SCHEME'] = 'http'
+    app.config['DISCORD_REDIRECT_URI'] = f'http://{app.config["SERVER_NAME"]}/callback'
+else:
+    app.config['PREFERRED_URL_SCHEME'] = 'https'
+    app.config['DISCORD_REDIRECT_URI'] = f'https://{app.config["SERVER_NAME"]}/callback'
+
 #loads in discord environment variables
-app.config['DISCORD_REDIRECT_URI'] = 'callback'
-for key in ['CLIENT_ID', 'CLIENT_SECRET', 'REDIRECT_URI']:
+for key in ['CLIENT_ID', 'CLIENT_SECRET']:
     key = 'DISCORD_' + key
     app.config[key] = os.environ[key]
 
